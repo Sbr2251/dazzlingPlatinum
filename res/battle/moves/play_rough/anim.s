2defe6a0a8f3990177e3a9599c1a480d27dbecea
Sudeep Reddy <sreddy9@meta.com>
2026-02-11 20:45:57 -0800

    Fix Play Rough animation freeze by using body_slam_spa
    charm_spa only has emitter 0; creating emitter 1 caused
    WaitForAllEmitters to hang. Switch to body_slam_spa which
    has both emitters and is designed for physical contact moves.

--- res/battle/moves/play_rough/anim.s
+++ res/battle/moves/play_rough/anim.s
@@ -1,17 +1,17 @@
 #include "macros/btlanimcmd.inc"
 
 L_0:
-    LoadParticleResource 0, charm_spa
-    PlaySoundEffectL SEQ_SE_DP_W036
+    LoadParticleResource 0, body_slam_spa
     Func_MoveBattler BATTLE_ANIM_BATTLER_SPRITE_ATTACKER, 0, 16, 4
     WaitForAnimTasks
     Func_MoveBattler BATTLE_ANIM_BATTLER_SPRITE_ATTACKER, 0, -16, 4
     WaitForAnimTasks
-    PlaySoundEffectR SEQ_SE_DP_W025B
+    PlaySoundEffectR SEQ_SE_DP_W036
+    PlayDelayedSoundEffectR SEQ_SE_DP_W025B, 6
     Func_MoveBattler BATTLE_ANIM_BATTLER_SPRITE_ATTACKER, 24, 0, 4
     WaitForAnimTasks
-    CreateEmitter 0, 0, EMITTER_CB_SET_POS_TO_DEFENDER
     CreateEmitter 0, 1, EMITTER_CB_SET_POS_TO_DEFENDER
+    CreateEmitter 0, 0, EMITTER_CB_SET_POS_TO_DEFENDER
     Func_FadeBattlerSprite BATTLE_ANIM_DEFENDER, 0, 1, BATTLE_COLOR_LIGHT_RED, 14, 0
     Func_Shake 3, 0, 1, 4, BATTLE_ANIM_BATTLER_SPRITE_DEFENDER
     Func_MoveBattler BATTLE_ANIM_BATTLER_SPRITE_ATTACKER, -24, 0, 4
